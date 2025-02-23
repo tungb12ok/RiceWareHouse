554a451395bddfae3df438479f6402048aab2042
Cáo Con <126157335+TungTranQuang271@users.noreply.github.com>
2025-02-23 23:34:49 +0700

    update

---
+++ build/web/home.jsp
@@ -0,0 +1,42 @@
+<%-- 
+    Document   : admin
+    Created on : Feb 23, 2025, 9:18:01 AM
+    Author     : Admin
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Home</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+
+        <%@include file="/components/header.jsp"%>
+
+        <div id="layoutSidenav">
+            
+
+
+            <%@include file="/components/footer.jsp"%>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+
+    </body>
+</html>
--- src/java/dao/CustomerDAO.java
+++ src/java/dao/CustomerDAO.java
@@ -27,8 +27,7 @@ public class CustomerDAO extends GenericDAO<Customer> {
     // Method to get customer by ID
     public Customer getCustomerById(int customerId) {
         String query = "SELECT * FROM Customers WHERE CustomerID = ?";
-        try (Connection conn = DatabaseConnection.getConnection(); 
-             PreparedStatement stmt = conn.prepareStatement(query)) {
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setInt(1, customerId);
             ResultSet rs = stmt.executeQuery();
 
@@ -44,36 +43,66 @@ public class CustomerDAO extends GenericDAO<Customer> {
     // Method to add a new customer
     public boolean addCustomer(Customer customer) {
         String query = "INSERT INTO Customers (FullName, Gender, Age, Address, PhoneNumber) VALUES (?, ?, ?, ?, ?)";
-        return executeUpdate(query, 
-                customer.getFullName(), 
-                customer.getGender(), 
-                customer.getAge(), 
-                customer.getAddress(), 
+        return executeUpdate(query,
+                customer.getFullName(),
+                customer.getGender(),
+                customer.getAge(),
+                customer.getAddress(),
                 customer.getPhoneNumber());
     }
 
     // Method to update customer information
     public boolean updateCustomer(Customer customer) {
         String query = "UPDATE Customers SET FullName = ?, Gender = ?, Age = ?, Address = ?, PhoneNumber = ? WHERE CustomerID = ?";
-        return executeUpdate(query, 
-                customer.getFullName(), 
+        return executeUpdate(query,
+                customer.getFullName(),
                 customer.getGender(),
-                customer.getAge(), 
-                customer.getAddress(), 
+                customer.getAge(),
+                customer.getAddress(),
                 customer.getPhoneNumber(),
                 customer.getCustomerId());
     }
 
     // Method to delete customer by ID
     public boolean deleteCustomer(int customerId) {
-        String query = "DELETE FROM Customers WHERE CustomerID = ?";
-        return executeUpdate(query, customerId);
+        // Start a transaction
+        try (Connection conn = DatabaseConnection.getConnection()) {
+            conn.setAutoCommit(false);  
+            String deleteHistoryQuery = "DELETE FROM TransactionHistory WHERE CustomerID = ?";
+            try (PreparedStatement psHistory = conn.prepareStatement(deleteHistoryQuery)) {
+                psHistory.setInt(1, customerId);
+                psHistory.executeUpdate();
+            }
+
+            String deleteTransactionsQuery = "DELETE FROM Transactions WHERE CustomerID = ?";
+            try (PreparedStatement psTransactions = conn.prepareStatement(deleteTransactionsQuery)) {
+                psTransactions.setInt(1, customerId);
+                psTransactions.executeUpdate();
+            }
+
+            String deleteCustomerQuery = "DELETE FROM Customers WHERE CustomerID = ?";
+            try (PreparedStatement psCustomer = conn.prepareStatement(deleteCustomerQuery)) {
+                psCustomer.setInt(1, customerId);
+                int affectedRows = psCustomer.executeUpdate();
+
+                if (affectedRows > 0) {
+                    conn.commit();
+                    return true;
+                } else {
+                    conn.rollback();
+                    return false;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     // Example usage for testing
     public static void main(String[] args) {
         CustomerDAO customerDAO = new CustomerDAO();
-        
+
         // Testing: Get all customers
         List<Customer> customers = customerDAO.getAllCustomers();
         for (Customer customer : customers) {
